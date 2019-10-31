1c73e80055df2838443f01e67ea4040e25c98b84
Pavel Taxidi <paveltaxidi@MacBook-Pro-Pavel.local>
2019-10-31 23:50:55 +0200

    lesson4 author-test added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_233232) do
+ActiveRecord::Schema.define(version: 2019_10_31_134708) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2019_10_19_233232) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
