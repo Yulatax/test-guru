a03ecd7fc5be09a537f0c199304882535ca97e39
Pavel Taxidi <paveltaxidi@mbp-pavel.mynet>
2019-10-31 14:04:44 +0200

    lesson4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  has_many :passing_tests
+  has_many :users, through: :passing_tests
 
   class << self
     def tests_by_category(category)
