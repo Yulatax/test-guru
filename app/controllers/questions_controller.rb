1cd84657ed3af547b152500c04545eacab04be7e
Pavel Taxidi <paveltaxidi@mbp-pavel.mynet>
2019-11-19 13:13:53 +0200

    lesson7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,12 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound,
               with: :rescue_with_question_not_found
 
   def index
-    @questions = @test.questions
+
   end
 
   def show
@@ -14,21 +14,34 @@ class QuestionsController < ApplicationController
   end
 
   def new
-
+    @question = @test.questions.new
   end
 
   def create
-    @question = @test.questions.create(question_params)
-    if @question.save
+    @question = Question.new(question_params)
+
+    if @test.questions.push(@question)
       redirect_to @question
     else
-      render plain: @question.inspect
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
   def destroy
     @question.destroy
-    render plane: 'Question was successfully removed'
+    redirect_to test_path(@question.test_id)
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  GITHUB = 'https://github.com'
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "#{GITHUB}/#{author}/#{repo}"
+  end
 end
