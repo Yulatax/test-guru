749548b9c0acbb96fd8116413985b7153cb10e91
Pavel Taxidi <paveltaxidi@MacBook-Pro-Pavel.local>
2019-12-09 00:57:16 +0200

    lesson9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to cookies[:redirect_path]
+      redirect_to cookies[:redirect_path] || root_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_134210) do
+ActiveRecord::Schema.define(version: 2019_12_08_225125) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_134210) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
